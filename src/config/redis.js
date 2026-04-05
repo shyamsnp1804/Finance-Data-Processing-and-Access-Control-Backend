ffe706ce6e87f5f2e90e32e07cd7d7ea72e5507a
shyamsnp1804 <snp18032004@gmail.com>
2026-04-05 09:58:54 +0530

    redis

--- src/config/redis.js
+++ src/config/redis.js
@@ -1,17 +1,30 @@
 const Redis = require("ioredis");
 
-const redis = new Redis({
-  host: process.env.REDIS_URL || "127.0.0.1",
-  port: process.env.REDIS_PORT || 6379,
-  password: process.env.REDIS_PASSWORD || undefined,
-});
+let redis;
 
-redis.on("connect", () => {
-  console.log("Redis connected");
-});
+if (process.env.NODE_ENV === "production") {
+  // Redis disable (safe deployment) 
+  redis = {
+    get: async () => null,
+    set: async () => null,
+  };
 
-redis.on("error", (err) => {
-  console.error("Redis error", err);
-});
+  console.log("Redis disabled in production");
+} else {
+  // real Redis in local development
+  redis = new Redis({
+    host: process.env.REDIS_URL || "127.0.0.1",
+    port: process.env.REDIS_PORT || 6379,
+    password: process.env.REDIS_PASSWORD || undefined,
+  });
+
+  redis.on("connect", () => {
+    console.log("Redis connected");
+  });
+
+  redis.on("error", (err) => {
+    console.error("Redis error", err);
+  });
+}
 
 module.exports = redis;
